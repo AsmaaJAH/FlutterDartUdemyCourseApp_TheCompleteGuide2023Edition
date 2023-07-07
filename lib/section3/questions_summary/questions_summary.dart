b74de142f480bb8a30e391bc916216af0cee67ec
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-07-07 15:52:30 +0300

    .toList()

--- lib/section3/questions_summary/questions_summary.dart
+++ lib/section3/questions_summary/questions_summary.dart
@@ -11,7 +11,7 @@ class QuestionsSummary extends StatelessWidget {
       height: 300,
       child: SingleChildScrollView(
         child: Column(
-          children: [...summaryData.map((item)=> SummaryItem(item))],
+          children: summaryData.map((item)=> SummaryItem(item)).toList(),
         ),
       ),
     );
