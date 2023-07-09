68c2b6cee1580fb420cfbb191fe819a5f28da4a1
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-07-09 17:43:32 +0300

    dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,27 @@ import 'package:course_app/section5_expense_tracker_app/expenses.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 1, 253, 203));
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 0, 114, 91),
+);
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardTheme(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         scaffoldBackgroundColor: const Color.fromARGB(255, 149, 245, 226),
@@ -19,8 +37,12 @@ void main() {
           color: Colors.white,
           margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         ),
-        elevatedButtonTheme:
-            ElevatedButtonThemeData(style: ElevatedButton.styleFrom()),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+            foregroundColor: kColorScheme.onPrimaryContainer,
+          ),
+        ),
         textTheme: ThemeData().textTheme.copyWith(
               titleLarge: TextStyle(
                 fontFamily: 'Roboto',
@@ -30,6 +52,7 @@ void main() {
               ),
             ),
       ),
+      //themeMode: ThemeMode.dark,
       home: const Expenses(),
     ),
   );
