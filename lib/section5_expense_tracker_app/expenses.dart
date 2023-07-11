7cd2c3fa8fe8b40458054b5b5bfec9677847145a
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-07-11 12:52:56 +0300

    auto rotate with MediaQuery.of(context)

--- lib/section5_expense_tracker_app/expenses.dart
+++ lib/section5_expense_tracker_app/expenses.dart
@@ -71,6 +71,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(context) {
+    final width = MediaQuery.of(context).size.width;
     Widget mainContent = const Center(
       child: Text("No Expenses Found. Start Entering your Expenses now! "),
     );
@@ -90,14 +91,25 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
+      body:width<600? Column(
         children: [
-          Chart(expenses: _registeredExpenses,),
+          Chart(
+            expenses: _registeredExpenses,
+          ),
           Expanded(
             child: mainContent,
           ),
         ],
-      ),
+      ): Row(children: [
+          Expanded(
+            child: Chart(
+              expenses: _registeredExpenses,
+            ),
+          ),
+          Expanded(
+            child: mainContent,
+          ),
+        ],),
     );
   }
 }
