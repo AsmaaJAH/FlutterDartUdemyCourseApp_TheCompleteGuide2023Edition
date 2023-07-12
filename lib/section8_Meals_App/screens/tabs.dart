d01aef8db424ec428adb04c688533941e1b38052
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-07-12 14:01:52 +0300

    drawer titles and  ListTile();

--- lib/section8_Meals_App/screens/tabs.dart
+++ lib/section8_Meals_App/screens/tabs.dart
@@ -1,6 +1,7 @@
 import 'package:course_app/section8_Meals_App/models/meal.dart';
 import 'package:course_app/section8_Meals_App/screens/categories.dart';
 import 'package:course_app/section8_Meals_App/screens/meals.dart';
+import 'package:course_app/section8_Meals_App/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
 class TabScreen extends StatefulWidget {
@@ -61,7 +62,9 @@ class _TabScreenState extends State<TabScreen> {
       );
     }
     return Scaffold(
+      drawer: const MainDrawer(),
       appBar: AppBar(
+        
         backgroundColor: Theme.of(context).colorScheme.onPrimary,
         title: Text(
           activePageTitle,
