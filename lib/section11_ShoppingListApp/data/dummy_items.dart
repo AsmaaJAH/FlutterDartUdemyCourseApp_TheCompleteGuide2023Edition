2cc11fcbe0c7278f70573bd362524aa7d50271a0
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-07-13 18:20:52 +0300

    section 11 challenge without Solution

---
+++ lib/section11_ShoppingListApp/data/dummy_items.dart
@@ -0,0 +1,22 @@
+import 'package:course_app/section11_ShoppingListApp/models/grocery_item.dart';
+import 'package:course_app/section11_ShoppingListApp/data/categories.dart';
+
+
+
+const groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
