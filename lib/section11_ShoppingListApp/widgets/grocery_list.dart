b4b4452c6905e3596e6e325161b91dd1d724720f
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-07-14 11:08:23 +0300

    ..

---
+++ lib/section11_ShoppingListApp/widgets/grocery_list.dart
@@ -0,0 +1,35 @@
+import 'package:course_app/section11_ShoppingListApp/data/dummy_items.dart';
+import 'package:flutter/material.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+  @override
+  State<GroceryList> createState() {
+    return _GroceryListState();
+  }
+}
+
+class _GroceryListState extends State<GroceryList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Groceries List"),
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(groceryItems[index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: groceryItems[index].category.color,
+          ),
+          trailing: Text(
+            groceryItems[index].quantity.toString(),
+          ),
+        ),
+      ),
+    );
+  }
+}
