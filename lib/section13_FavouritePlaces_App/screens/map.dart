b7daa78905c1fdb208af7e6452481b85b0661471
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-07-17 14:53:18 +0300

    section 14

--- lib/section13_FavouritePlaces_App/screens/map.dart
+++ lib/section13_FavouritePlaces_App/screens/map.dart
@@ -1,6 +1,6 @@
-import 'package:course_app/section13_FavouritePlaces_App/models/place.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:course_app/section13_FavouritePlaces_App/models/place.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({
