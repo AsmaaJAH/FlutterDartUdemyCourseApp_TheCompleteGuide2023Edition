b3f02bccdc780600195955b1ec7f401d44d68671
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-07-18 13:28:55 +0300

    chat sending and receiving on screen

--- lib/section14_chatApp/widgets/chat_messages.dart
+++ lib/section14_chatApp/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,8 +6,44 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No messages found yet! start adding some.'),
-    );
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats')
+            .orderBy(
+              'createdTime',
+              descending: false,
+            )
+            .snapshots(),
+        builder: (ctx, chatSnapshots) {
+          if (chatSnapshots.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final loadedMessages = chatSnapshots.data!.docs;
+
+          if (!chatSnapshots.hasData || loadedMessages.isEmpty) {
+            return const Center(
+              child: Text('No messages found yet! start adding some.'),
+            );
+          }
+          if (chatSnapshots.hasError) {
+            return const Center(
+              child: Text('Something went wrong...'),
+            );
+          }
+          return ListView.builder(
+            padding: const EdgeInsets.only(
+              bottom: 40,
+              left: 15,
+              right: 15,
+            ),
+            itemCount: loadedMessages.length,
+            itemBuilder: (ctx, index) => Text(
+              loadedMessages[index].data()['text'],
+            ),
+          );
+        });
   }
 }
--- lib/section14_chatApp/widgets/new_message.dart
+++ lib/section14_chatApp/widgets/new_message.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final _firebaseAuth = FirebaseAuth.instance;
+final _firebaseFirestore = FirebaseFirestore.instance;
 
 class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
@@ -17,13 +22,26 @@ class _NewMessageState extends State<NewMessage> {
     _newMessageController.dispose();
   }
 
-  void _submitMessage() {
+  void _submitMessage() async {
     final enteredMessage = _newMessageController.text;
     if (enteredMessage.trim().isEmpty) {
       return;
     }
-
+    FocusScope.of(context).unfocus();
     _newMessageController.clear();
+    final currentUser = _firebaseAuth.currentUser!;
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(currentUser.uid)
+        .get();
+
+    _firebaseFirestore.collection('chats').add({
+      'text': enteredMessage,
+      'createdTime': Timestamp.now(),
+      'userId': currentUser.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['imageURL'],
+    });
   }
 
   @override
